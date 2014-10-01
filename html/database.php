3ce55b19ef4b5d25640261930b7fbe462d75aa41
Jorge Garin <jorge.garinr@alumnos.uv.cl>
2014-10-01 02:03:18 -0300

    cambios en base de datos

--- html/database.php
+++ html/database.php
@@ -45,38 +45,31 @@
     <section id="about-us" class="container">
         <div class="row">
             <div class="col-sm-6">
-                <h2>What we jhfjhghg</h2>
+                <h2>Gestión y Diseño de Base de Datos</h2>
                 <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.</p>
             </div><!--/.col-sm-6-->
             <div class="col-sm-6">
-                <h2>Our Skills</h2>
-                <div>
-                    <div class="progress">
-                        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%;">
-                            <span>HTML/CSS</span>
-                        </div>
-                    </div>
-                    <div class="progress">
-                        <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%;">
-                            <span>Wordpress</span>
-                        </div>
-                    </div>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">
-                            <span>Joomla</span>
-                        </div>
-                    </div>
-                    <div class="progress">
-                        <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" style="width: 55%;">
-                            <span>Drupal</span>
-                        </div>
-                    </div>
-                    <div class="progress">
-                        <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
-                            <span>PHP</span>
-                        </div>
-                    </div>
-                </div>
+                <h2></h2>
+                <section id="main-slider" class="no-margin">
+                        <div class="carousel slide wet-asphalt">
+                            <ol class="carousel-indicators">
+                                <li data-target="#main-slider" data-slide-to="0" class="active"></li>
+                                <li data-target="#main-slider" data-slide-to="1"></li>
+                                <li data-target="#main-slider" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="item active fotos_mencion" style="background-image: url(../images/baltica.jpg)"></div><!--/.item-->
+                                <div class="item fotos_mencion" style="background-image: url(../images/baltica.jpg)"></div><!--/.item-->
+                                <div class="item fotos_mencion" style="background-image: url(../images/baltica.jpg)"></div><!--/.item-->
+                            </div><!--/.carousel-inner-->
+                        </div><!--/.carousel-->
+                        <a class="prev hidden-xs" href="#main-slider" data-slide="prev">
+                            <i class="icon-angle-left"></i>
+                        </a>
+                        <a class="next hidden-xs" href="#main-slider" data-slide="next">
+                            <i class="icon-angle-right"></i>
+                        </a>
+                    </section><!--/#main-slider-->
             </div><!--/.col-sm-6-->
         </div><!--/.row-->
 
