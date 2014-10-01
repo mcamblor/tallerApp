787439eeb6d14e780ebb060605d984f57093ad82
Jorge Garin <jorge.garinr@alumnos.uv.cl>
2014-10-01 02:01:40 -0300

    cambio csm

--- html/software.php
+++ html/software.php
@@ -45,7 +45,7 @@
     <section id="about-us" class="container">
         <div class="row">
             <div class="col-sm-6">
-                <h2>Gestión de Software</h2>
+                <h2>Gestión de Proyectos de Software</h2>
                 <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.</p>
             </div><!--/.col-sm-6-->
                 <div class="col-sm-6">
