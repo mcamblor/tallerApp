038617e89458f39cabcd1eae09157a4689f2f623
Jorge Garin <jorge.garinr@alumnos.uv.cl>
2014-10-01 03:43:57 -0300

    cambios en las menciones

--- html/database.php
+++ html/database.php
@@ -58,9 +58,9 @@
                                 <li data-target="#main-slider" data-slide-to="2"></li>
                             </ol>
                             <div class="carousel-inner">
-                                <div class="item active fotos_mencion" style="background-image: url(../images/baltica.jpg)"></div><!--/.item-->
-                                <div class="item fotos_mencion" style="background-image: url(../images/baltica.jpg)"></div><!--/.item-->
-                                <div class="item fotos_mencion" style="background-image: url(../images/baltica.jpg)"></div><!--/.item-->
+                                <div class="item active fotos_mencion" style="background-image: url(../images/mencion/data1.png)"></div><!--/.item-->
+                                <div class="item fotos_mencion" style="background-image: url(../images/mencion/data2.png)"></div><!--/.item-->
+                                <div class="item fotos_mencion" style="background-image: url(../images/mencion/data3.png)"></div><!--/.item-->
                             </div><!--/.carousel-inner-->
                         </div><!--/.carousel-->
                         <a class="prev hidden-xs" href="#main-slider" data-slide="prev">
