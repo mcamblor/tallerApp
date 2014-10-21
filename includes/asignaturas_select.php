6bdf988cd508f5efd48137d72a5a1ac667cd0b3a
Jorge Garin <jorge.garinr@alumnos.uv.cl>
2014-10-20 21:25:27 -0300

    arreglos varios

--- includes/asignaturas_select.php
+++ includes/asignaturas_select.php
@@ -7,80 +7,80 @@
     <option value="sem1_inc104">Formación Valórica Personal</option>
 </optgroup>
 <optgroup label="Semestre II">
-	<option>Física</option>
-    <option>Cálculo Integral</option>
-    <option>Programación I</option>
-    <option>Introducción al Hardware</option>
-    <option>Filosofía de las Ciencias</option>
+	<option value="sem2_inc110">Física</option>
+    <option value="sem2_inc111">Cálculo Integral</option>
+    <option value="sem2_inc112">Programación I</option>
+    <option value="sem2_inc113">Introducción al Hardware</option>
+    <option value="sem2_inc114">Filosofía de las Ciencias</option>
 </optgroup>
 <optgroup label="Semestre III">
-	<option>Física Experimental</option>
-    <option>Cálculo Multivariable</option>
-    <option>Programación II</option>
-    <option>Sistemas Digitales</option>
-    <option>Estructuras Discretas</option>
+	<option value="sem3_inc200">Física Experimental</option>
+    <option value="sem3_inc201">Cálculo Multivariable</option>
+    <option value="sem3_inc202">Programación II</option>
+    <option value="sem3_inc203">Sistemas Digitales</option>
+    <option value="sem3_inc204">Estructuras Discretas</option>
 </optgroup>
 <optgroup label="Semestre IV">
-	<option>Algebra Lineal</option>
-    <option>Estructura de Datos</option>
-    <option>Arquitectura de Computadores</option>
-    <option>Teoría de Sistemas</option>
-    <option>Contabilidad</option>
+	<option value="sem4_inc210">Algebra Lineal</option>
+    <option value="sem4_inc211">Estructura de Datos</option>
+    <option value="sem4_inc212">Arquitectura de Computadores</option>
+    <option value="sem4_inc213">Teoría de Sistemas</option>
+    <option value="sem4_inc214">Contabilidad</option>
 </optgroup>
 <optgroup label="Semestre V">
-	<option>Ecuaciones Diferenciales</option>
-    <option>Electro-magnetismo</option>
-    <option>Análisis y Diseño de Algoritmos</option>
-    <option>Teoría de Sistemas Operativos</option>
-    <option>Fundamentos de Ingeniería de Software</option>
-    <option>Ingles I</option>	
+	<option value="sem5_inc300">Ecuaciones Diferenciales</option>
+    <option value="sem5_inc301">Electro-magnetismo</option>
+    <option value="sem5_inc302">Análisis y Diseño de Algoritmos</option>
+    <option value="sem5_inc303">Teoría de Sistemas Operativos</option>
+    <option value="sem5_inc304">Fundamentos de Ingeniería de Software</option>
+    <option value="sem5_inc305">Ingles I</option>	
 </optgroup>
 <optgroup label="Semestre VI">
-	<option>Probabilidad y Estadística</option>
-    <option>Lenguajes y Automatas</option>
-    <option>Redes de Computadores</option>
-    <option>Metodologías de Análisis</option>
-    <option>Administración en Informática</option>
-    <option>Ingles II</option>
+	<option value="sem6_inc310">Probabilidad y Estadística</option>
+    <option value="sem6_inc311">Lenguajes y Automatas</option>
+    <option value="sem6_inc312">Redes de Computadores</option>
+    <option value="sem6_inc313">Metodologías de Análisis</option>
+    <option value="sem6_inc314">Administración en Informática</option>
+    <option value="sem6_inc315">Ingles II</option>
 </optgroup>
 <optgroup label="Semestre VII">
-	<option>Física Contemporanea</option>
-    <option>Interfaz Hombre-Máquina</option>
-    <option>Modelos de Datos</option>
-    <option>Taller de Sistemas Operativos</option>
-    <option>Metodologías de Diseño</option>
-    <option>Ingles III</option>
+	<option value="sem7_inc400">Física Contemporanea</option>
+    <option value="sem7_inc401">Interfaz Hombre-Máquina</option>
+    <option value="sem7_inc402">Modelos de Datos</option>
+    <option value="sem7_inc403">Taller de Sistemas Operativos</option>
+    <option value="sem7_inc404">Metodologías de Diseño</option>
+    <option value="sem7_inc405">Ingles III</option>
 </optgroup>
 <optgroup label="Semestre VIII">
-	<option>Biología</option>
-    <option>Desarrollo Web</option>
-    <option>Sistemas de Bases de Datos</option>
-    <option>Arquitectura de Software</option>
-    <option>Evaluación de Proyectos</option>
-    <option>Lenguajes de Programación</option>
+	<option value="sem8_inc410">Biología</option>
+    <option value="sem8_inc411">Desarrollo Web</option>
+    <option value="sem8_inc412">Sistemas de Bases de Datos</option>
+    <option value="sem8_inc413">Arquitectura de Software</option>
+    <option value="sem8_inc414">Evaluación de Proyectos</option>
+    <option value="sem8_inc415">Lenguajes de Programación</option>
 </optgroup>
 <optgroup label="Semestre IX">
-	<option>Pruebas de Software</option>
-    <option>Seminario de Especialidad I</option>
-    <option>Asignatura Electiva de Especialidad I</option>
-    <option>Sistemas de Telecomunicaciones</option>
-    <option>Gestion de Proyectos Informáticos</option>
-    <option>Investigacíon de Operaciones</option>
+	<option value="sem9_inc500">Pruebas de Software</option>
+    <option value="sem9_inc501">Seminario de Especialidad I</option>
+    <option value="sem9_inc502">Asignatura Electiva de Especialidad I</option>
+    <option value="sem9_inc503">Sistemas de Telecomunicaciones</option>
+    <option value="sem9_inc504">Gestion de Proyectos Informáticos</option>
+    <option value="sem9_inc505">Investigacíon de Operaciones</option>
 </optgroup>
 <optgroup label="Semestre X">
-	<option>Economía</option>
-    <option>Seminario de Especialidad II</option>
-    <option>Asignatura Electiva de Especialidad II</option>
-    <option>Fundamentos de Inteligencia Artificial</option>
-    <option>Taller de Aplicaciones</option>
-    <option>Simulación</option>
+	<option value="sem10_inc510">Economía</option>
+    <option value="sem10_inc511">Seminario de Especialidad II</option>
+    <option value="sem10_inc512">Asignatura Electiva de Especialidad II</option>
+    <option value="sem10_inc513">Fundamentos de Inteligencia Artificial</option>
+    <option value="sem10_inc514">Taller de Aplicaciones</option>
+    <option value="sem10_inc515">Simulación</option>
 </optgroup>
 <optgroup label="Semestre XI">
-	<option>Asignatura Electiva de Especialidad III</option>
-    <option>Seminario de Título I</option>
-    <option>Etica y Legislación</option>
+	<option value="sem11_inc600">Asignatura Electiva de Especialidad III</option>
+    <option value="sem11_inc601">Seminario de Título I</option>
+    <option value="sem11_inc602">Etica y Legislación</option>
 </optgroup>
 <optgroup label="Semestre XII">
-	<option>Seminario de Tecnología de Información y Comunicación</option>
-    <option>Seminario de Título II</option>
+	<option value="sem12_inc611">Seminario de Tecnología de Información y Comunicación</option>
+    <option value="sem12_inc612">Seminario de Título II</option>
 </optgroup>
