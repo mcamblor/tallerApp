8e98b65d17e4bb3b427af4e7f15a03a043a1f777
jgarinr <garan2sisisi@gmail.com>
2014-11-27 09:33:16 -0300

    cambios pequeños

--- includes/header_html.php
+++ includes/header_html.php
@@ -10,7 +10,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="../index.php"><img src="../images/banner/logo_header.png"></a>
+                <a class="navbar-brand" href="../index.php">Informática UV</a>
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
